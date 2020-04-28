adead885bfec51dd0a13f8df948be31c27778507
Tess Gronski <theresegronski@Tesss-MacBook-Air.local>
2020-04-28 00:08:20 -0700

    Edits WalletService

--- src/wallets/wallets-service.js
+++ src/wallets/wallets-service.js
@@ -1,10 +1,10 @@
-const CalculationsService = {
+const WalletsService = {
     getById(knex, user_id ) {
       return knex
         .from("networth_wallet")
         .select("*")
         // .where("user_id", user_id)
-        .first();
+        // .first();
 
   }
 }
