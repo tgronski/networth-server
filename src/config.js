f6a333cfe8d1119ab6e808fdfbf8fd34a8a3e450
Tess Gronski <theresegronski@Tesss-MacBook-Air.local>
2020-04-29 15:48:26 -0700

    adds values to config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
     PORT: process.env.PORT,
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
-    JWT_SECRET: process.env.JWT_SECRET,
-    JWT_EXPIRY: process.env.JWT_EXPIRY,
+    JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '15m',
 
 }
