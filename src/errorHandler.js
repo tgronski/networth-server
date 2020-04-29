9234257090facee151ee973030fc24f794f6f95c
Tess Gronski <theresegronski@Tesss-MacBook-Air.local>
2020-04-29 15:46:20 -0700

    Removes error handler

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -4,12 +4,12 @@ const logger = require('./logger')
 
 function errorHandler(error, req, res, next) {
   let response
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } }
-  } else {
+  // if (NODE_ENV === 'production') {
+  //   response = { error: { message: 'server error' } }
+  // } else {
     logger.error(error.message)
     response = { message: error.message, error }
-  }
+  // }
   res.status(500).json(response)
 }
 
