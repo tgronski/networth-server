e2984e1938b28543d48e7b70a714b9f7d807ce30
Tess Gronski <theresegronski@Tesss-MacBook-Air.local>
2020-04-29 20:45:33 -0700

    Adds value to JWT expiry (config)

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-    JWT_EXPIRY: process.env.JWT_EXPIRY,
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '30m',
 
 }
